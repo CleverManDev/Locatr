e186ad2b747afccb058626d27422fcfac62495c7
clever_man <clever.man.dev@gmail.com>
2017-02-10 13:06:14 +0300

    Chapter 31 - Locations and Play Services - Getting a Location Fix

--- app/src/main/java/android/bignerdranch/com/locatr/LocatrFragment.java
+++ app/src/main/java/android/bignerdranch/com/locatr/LocatrFragment.java
@@ -1,16 +1,30 @@
 package android.bignerdranch.com.locatr;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
 public class LocatrFragment extends Fragment {
+	private static final String TAG = "LocatrFragment";
 	private ImageView mImageView;
+	private GoogleApiClient mClient;
 
 	public static LocatrFragment newInstance() {
 		return new LocatrFragment();
@@ -20,6 +34,20 @@ public class LocatrFragment extends Fragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setHasOptionsMenu(true);
+		mClient = new GoogleApiClient.Builder(getActivity())
+				.addApi(LocationServices.API)
+				.addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+					@Override
+					public void onConnected(Bundle bundle) {
+						getActivity().invalidateOptionsMenu();
+					}
+
+					@Override
+					public void onConnectionSuspended(int i) {
+
+					}
+				})
+				.build();
 	}
 
 	@Override
@@ -30,8 +58,61 @@ public class LocatrFragment extends Fragment {
 	}
 
 	@Override
+	public void onStart() {
+		super.onStart();
+		getActivity().invalidateOptionsMenu();
+		mClient.connect();
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		mClient.disconnect();
+	}
+
+	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.fragment_locatr, menu);
+		MenuItem searchItem = menu.findItem(R.id.action_locate);
+		searchItem.setEnabled(mClient.isConnected());
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_locate:
+				findImage();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void findImage() {
+		LocationRequest request = LocationRequest.create();
+		request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		request.setNumUpdates(1);
+		request.setInterval(0);
+		if (ActivityCompat.checkSelfPermission(getActivity(),
+				android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		LocationServices.FusedLocationApi
+				.requestLocationUpdates(mClient, request, new LocationListener() {
+					@Override
+					public void onLocationChanged(Location location) {
+						Log.i(TAG, "Got a fix: " + location);
+					}
+				});
+	}
+
+
 }
